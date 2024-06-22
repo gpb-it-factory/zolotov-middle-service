fe14c9b906016caeb1d4aa98681a7042bb6bf1f5
Sanf1r <sanfir@yandex.ru>
2024-06-22 16:45:47 +0300

    Delete restTemplate in favor of webServer, add timeout on request, change api endpoint

--- src/main/kotlin/ru/gpb/midserv/models/User.kt
+++ src/main/kotlin/ru/gpb/midserv/models/User.kt
@@ -1,6 +1,3 @@
 package ru.gpb.midserv.models
 
-import kotlinx.serialization.Serializable
-
-@Serializable
 data class User(val userId: Long, val userName: String)
--- src/main/kotlin/ru/gpb/midserv/service/ServerApiResponse.kt
+++ src/main/kotlin/ru/gpb/midserv/service/ServerApiResponse.kt
@@ -1,16 +1,22 @@
 package ru.gpb.midserv.service
 
-import kotlinx.serialization.Serializable
 
 sealed class ServerApiResponse {
-    data class Success(val message: String = "Success") : ServerApiResponse()
+    data class Success(
+        val message: String = "Success"
+    ) : ServerApiResponse()
 
-    data class Problem(val message: String, val type: String, val code: String, val traceId: String) :
-        ServerApiResponse()
+    data class Problem(
+        val message: String = "",
+        val type: String = "",
+        val code: String = "",
+        val traceId: String = ""
+    ) : ServerApiResponse()
 
-    data class Error(val message: String, val type: String, val code: String, val traceId: String) :
-        ServerApiResponse()
+    data class Error(
+        val message: String = "",
+        val type: String = "",
+        val code: String = "",
+        val traceId: String = ""
+    ) : ServerApiResponse()
 }
-
-@Serializable
-data class Error(val message: String = "", val type: String = "", val code: String = "", val traceId: String = "")
