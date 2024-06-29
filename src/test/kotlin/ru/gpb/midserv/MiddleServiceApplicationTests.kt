a6ab246010b24ab36804aa0393c42ade7bd21a74
Sanf1r <sanfir@yandex.ru>
2024-06-29 18:46:48 +0300

    Add tests + github actions CI

--- src/test/kotlin/ru/gpb/midserv/MiddleServiceApplicationTests.kt
+++ src/test/kotlin/ru/gpb/midserv/MiddleServiceApplicationTests.kt
@@ -1,13 +1,50 @@
 package ru.gpb.midserv
 
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
+import ru.gpb.midserv.controller.RegisterController
+import ru.gpb.midserv.models.User
+import ru.gpb.midserv.service.RegisterService
+import ru.gpb.midserv.service.ServerApiResponse
 
 @SpringBootTest
 class MiddleServiceApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun testRegister() {
+
+        var testCount = 1
+        val testUser = User(21, "Pabl0")
+        val client = mockk<WebClient>()
+        val service = RegisterService(client)
+        val controller = RegisterController(service)
+
+        every {
+            client
+                .post()
+                .uri("/v2/users")
+                .contentType(any())
+                .body(any(), User::class.java)
+                .exchangeToMono<ServerApiResponse>(any())
+                .timeout(any())
+        } answers {
+            when (testCount++) {
+                1 -> Mono.just(ServerApiResponse.Success())
+                2 -> Mono.just(ServerApiResponse.Problem())
+                else -> Mono.just(ServerApiResponse.Error())
+            }
+        }
+
+
+        Assertions.assertEquals(HttpStatus.NO_CONTENT, controller.receiveUser(testUser).statusCode)
+        Assertions.assertEquals(HttpStatus.CONFLICT, controller.receiveUser(testUser).statusCode)
+        Assertions.assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, controller.receiveUser(testUser).statusCode)
+    }
 
 }
